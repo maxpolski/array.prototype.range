4002c87f42a6aa2e55ed826e002c8b320dc4eb2c
Max Korabekskyi <maksym_korabelskyi@epam.com>
2018-02-08 11:33:04 +0200

    add range param

--- polyfill.js
+++ polyfill.js
@@ -1,11 +1,17 @@
 (function() {
-  Array.prototype.range = function(start, end) {
+  Array.prototype.range = function(start, end, step) {
     if (arguments.length === 1) {
       end = start;
       start = 0;
+      step = 1;
     }
-    return Array.apply(null, { length: end - start }).map(function(val, index) {
-      return Number.call(Number, index + start);
+
+    if (arguments.length === 2) {
+      step = 1;
+    }
+
+    return Array.apply(null, { length: ((end - (start + 1)) / step) + 1 }).map(function(val, index) {
+      return Number.call(Number, step * index + start);
     });
   }
 }());
